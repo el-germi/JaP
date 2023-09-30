9abdda56b6420b5589b63008800086c8ac36f243
German Maximo Montero Sande <german.montero@fing.edu.uy>
2023-09-30 18:17:45 -0300

    limpieza y organizacion

--- js/product-info.js
+++ js/product-info.js
@@ -7,8 +7,8 @@ const URLComentarios = PRODUCT_INFO_COMMENTS_URL + prodID + EXT_TYPE
 function mostrarComentarios() {
     let contenidoHTML = ""
     comentarios.forEach((comentario) => {
-        contenidoHTML += 
-        `<div class="card mb-3">
+        contenidoHTML +=
+            `<div class="card mb-3">
             <div class="card-body">
                 <div class="d-flex">
                     <div class="w-100">
@@ -142,15 +142,14 @@ function mostrarDatosDelProducto(productData) {
     let carouselBtns = document.getElementById("carouselBtns");
 
     for (let i = 0; i < productData.images.length; i++) {
-        carouselImgs.innerHTML+=`<div class="carousel-item">
+        carouselImgs.innerHTML += `<div class="carousel-item">
             <img src="${productData.images[i]}" class="d-block w-100">
         </div>`
-        carouselBtns.innerHTML+=`<button type="button" data-bs-target="#carouselExampleIndicators"
-        data-bs-slide-to="${i}" aria-label="Slide ${i+1}"></button>`
-        
+        carouselBtns.innerHTML += `<button type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="${i}" aria-label="Slide ${i + 1}"></button>`
+
     }
-    console.log(carouselBtns.childNodes)
-    carouselBtns.childNodes[0].setAttribute("aria-current","true");
+    carouselBtns.childNodes[0].setAttribute("aria-current", "true");
     carouselBtns.childNodes[0].classList.add("active");
     carouselImgs.childNodes[0].classList.add("active");
 
@@ -193,8 +192,8 @@ const isDarkMode = localStorage.getItem("darkMode") === "true";
 
 // Aplica el modo oscuro si estaba activado
 if (isDarkMode) {
-  document.body.classList.add("dark");
-  btnSwitch.classList.add("active");
+    document.body.classList.add("dark");
+    btnSwitch.classList.add("active");
 }
 
 // Agrega un evento de clic al botón
