8b0688a0e1dfa44191174398246b3c0cec24a8b2
HuguitoDeM <hugojavierdesideriomatinez@gmail.com>
2023-09-30 17:53:38 -0300

    my profile con funcionalidad de dark mode(faltaba ese)

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,8 +1,23 @@
 let btnSwitch = document.getElementById("switch");
 
-btnSwitch.addEventListener("click", ()=>{
-    document.body.classList.toggle("dark")
-    btnSwitch.classList.toggle("active")
-})
+// Recupera el estado del modo oscuro desde el localStorage
+const isDarkMode = localStorage.getItem("darkMode") === "true";
 
+// Aplica el modo oscuro si estaba activado
+if (isDarkMode) {
+  document.body.classList.add("dark");
+  btnSwitch.classList.add("active");
+}
 
+// Agrega un evento de clic al botón
+btnSwitch.addEventListener("click", () => {
+    // Alterna la clase "dark" en el cuerpo del documento
+    document.body.classList.toggle("dark");
+
+    // Alterna la clase "active" en el propio botón
+    btnSwitch.classList.toggle("active");
+
+    // Guarda el estado actual del modo oscuro en el localStorage
+    const isDarkModeActive = document.body.classList.contains("dark");
+    localStorage.setItem("darkMode", isDarkModeActive);
+});
