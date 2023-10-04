509861a74cc8e52c7112ecc619bb8305a9b91078
HuguitoDeM <hugojavierdesideriomatinez@gmail.com>
2023-10-04 15:26:58 -0300

    parte 2 casi terminada y tocando cosas de parte 3

--- js/cart.js
+++ js/cart.js
@@ -11,54 +11,72 @@ if (isDarkMode) {
 
 // Agrega un evento de clic al botón
 btnSwitch.addEventListener("click", () => {
-    // Alterna la clase "dark" en el cuerpo del documento
-    document.body.classList.toggle("dark");
+  // Alterna la clase "dark" en el cuerpo del documento
+  document.body.classList.toggle("dark");
 
-    // Alterna la clase "active" en el propio botón
-    btnSwitch.classList.toggle("active");
+  // Alterna la clase "active" en el propio botón
+  btnSwitch.classList.toggle("active");
 
-    // Guarda el estado actual del modo oscuro en el localStorage
-    const isDarkModeActive = document.body.classList.contains("dark");
-    localStorage.setItem("darkMode", isDarkModeActive);
+  // Guarda el estado actual del modo oscuro en el localStorage
+  const isDarkModeActive = document.body.classList.contains("dark");
+  localStorage.setItem("darkMode", isDarkModeActive);
 });
 
-let usuario="25801";
-let URL= `https://japceibal.github.io/emercado-api/user_cart/${usuario}.json`;
+
+// ---------------------------------------
+// FUNCIONALIDAD DEL CARRITO
+// ---------------------------------------
+
+let cartInfo = []
+let usuario = "25801";
+let URL = `https://japceibal.github.io/emercado-api/user_cart/${usuario}.json`;
 
 fetch(URL)
-.then(res => {
-   if (res.ok) {
-   return res.json();
-    }})
-.then(res =>{
-  let productos = res.articles;
-  let prodLocal = JSON.parse(localStorage.getItem("prodsCarrito")) || []
-  console.log(productos)
-  console.log(prodLocal)
-  let masProds = productos.concat(prodLocal)
-
-  console.log(masProds)
-  MostrarDataProductos(masProds)
-}  )
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(res => {
+    let productos = res.articles;
+    let prodLocal = JSON.parse(localStorage.getItem("prodsCarrito")) || []
+    let masProds = productos.concat(prodLocal)
+    cartInfo = masProds;
+    MostrarDataProductos()
+  })
 
 
-function MostrarDataProductos(dataArray) {
+function MostrarDataProductos() {
   productos.innerHTML = "";
-  for (const item of dataArray) {
-    
-      productos.innerHTML += `
-        <img src=${item.image}>
-        <div class="nombre">
+  for (let i = 1; i < cartInfo.length + 1; i++) {
+    const item = cartInfo[i - 1];
+    productos.innerHTML += `
+        
+       <div class ="nombre">
+       <div class="achicar"><img class="imgchiquita" src=${item.image}></div>  
           <h2>${item.name}</h2>
-          <p class="descripcion">${item.count}</p>
           <p>${item.currency} ${item.unitCost} </p>
-          <input type="number" id"number" value="1" min=0>
-          <p> <strong> ${item.currency} ${item.unitCost} </strong></p> 
-          
+          <input type="number" id="number${i}" value="${item.count}" attr_id="${i}" min=0>
+          <p id="subtotal${i}"> <strong> ${item.currency}</strong></p> 
         </div>
       </div>
 
       `
+    document.getElementById("number" + i).addEventListener("change", updateValues);
   }
-}  
+}
 
+
+function updateValues(evento) {
+  let i = evento.target.getAttribute("attr_id");
+  console.log(i);
+  document.getElementById("subtotal" + i).innerHTML = document.getElementById("number" + i).value * cartInfo[i-1].unitCost;
+
+  //let cantidad = e.source.value;
+  //let cuenta = precio * cantidad
+
+  //return cuenta;
+
+}
+
+setInterval(updateValues, 500);
