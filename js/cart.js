e5054ec1258e943e10326c052535f86d5441263c
HuguitoDeM <hugojavierdesideriomatinez@gmail.com>
2023-10-04 14:10:30 -0300

    trabajando funcionalidad del carrito

--- js/cart.js
+++ js/cart.js
@@ -21,3 +21,40 @@ btnSwitch.addEventListener("click", () => {
     const isDarkModeActive = document.body.classList.contains("dark");
     localStorage.setItem("darkMode", isDarkModeActive);
 });
+
+let usuario="25801";
+let URL= `https://japceibal.github.io/emercado-api/user_cart/${usuario}.json`;
+
+fetch(URL)
+.then(res => {
+   if (res.ok) {
+   return res.json();
+    }})
+.then(res =>{
+  console.log(res.articles)
+  let productos = res.articles;
+  productos.concat(JSON.parse(localStorage.getItem("prodsCarrito")) || [])
+  MostrarDataProductos(productos)
+}  )
+
+
+function MostrarDataProductos(dataArray) {
+  productos.innerHTML = "";
+  
+  for (const item of dataArray) {
+      productos.innerHTML += `
+        <img src=${item.image}>
+        <div class="contenido">
+          <h2>${item.name}</h2>
+          <p class="descripcion">${item.count}</p>
+          <p>${item.currency} ${item.unitCost} </p>
+          <input type="number" value="1" min=0 onchange>
+          <p> ${item.unitCost}</p>
+          
+        </div>
+      </div>`
+  
+      
+  }
+}
+
